8692222a236dbf1f4d63e3cdc1d3827c19a3a9d9
Sebas2545 <sjgonzalez00@gmail.com>
2022-02-18 20:21:36 -0800

    testing commits

--- pages/myProfile/index.js
+++ pages/myProfile/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ProfileView() {
     return (<>
         <h1>
-            Welcome user!
+            Welcome Seb!
             <b/>
             This is working
         </h1>
