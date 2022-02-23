8a6177b9d59ec2289fe0f898eec9a3f7a9f10e28
unknown <joltixfire>
2022-02-23 01:10:50 -0800

    My first commit

--- pages/myProfile/index.js
+++ pages/myProfile/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ProfileView() {
     return (<>
         <h1>
-            Welcome Seb!
+            Welcome Josh!
             <b/>
             This is working
         </h1>
