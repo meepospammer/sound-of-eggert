6a6eb8a4d8b4c4888b6f5d5d706d41bd6ac649c4
Elias Berner <theepicelias@hotmail.com>
2022-02-26 20:55:40 -0800

    Added welcome / homepage component and did some basic formatting

---
+++ pages/components/welcome/welcome.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './welcome.module.css'
+
+function Welcome(){
+    return(
+        <box className = {styles.box}>
+            <div className={styles.div}>
+                <h1>
+                    Sound of Eggert!
+                </h1>
+                Welcome to the sound of Eggert project!
+                <br/>
+                Our webapp makes ranking and browsing music easy.
+            </div>
+        </box>
+    )
+}
+
+export default Welcome;
