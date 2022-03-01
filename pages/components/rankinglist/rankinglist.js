85c0b5969b05cfa54a81fda935f5628cdbe5ac53
Elias Berner <theepicelias@hotmail.com>
2022-03-01 15:06:24 -0800

    Updated rankinglist component format as was planned earlier

--- pages/components/rankinglist/rankinglist.js
+++ pages/components/rankinglist/rankinglist.js
@@ -4,14 +4,18 @@ import ClickableAlbum from '../clickablealbum/clickablealbum';
 
 function rankinglist(){
     return(
+        //Sort albums by ranking and then display in order
         <ul className={styles.ul}>
-            texr
             <ClickableAlbum name="Gluee" artist="Bladee"
              src="https://i1.sndcdn.com/artworks-000068528208-1w4h5h-t500x500.jpg"
              rating={10.0}/>
 
-             <ClickableAlbum name="graduation" artist="Ye"
+             <ClickableAlbum name="Graduation" artist="Ye"
              src="https://upload.wikimedia.org/wikipedia/en/7/70/Graduation_%28album%29.jpg" rating={8.4}/>
+
+             <ClickableAlbum name="Donda" artist="Ye"
+             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Solid_black.svg/1920px-Solid_black.svg.png"
+             rating={7.9}/>
         </ul>
     )
 }
