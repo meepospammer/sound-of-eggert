06edec3f2258c954055aa1c17df4caedf64b7298
Elias Berner <theepicelias@hotmail.com>
2022-03-05 23:01:58 -0800

    Added functionality to leave comments in commentsection component
    Created basic structure for the component which allows users to add comments through the UI provided by the commentsection component. This needs to now be integrated with the backend

--- pages/components/commentsection/commentsection.js
+++ pages/components/commentsection/commentsection.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import styles from './commentsection.module.css'
+import { useState } from 'react';
+import { Component } from 'react';
 
 var comments = ["I really enjoyed this project! The production was outstanding",
             "The  artist really delivered and surpassed expectations",
             "I'm feeling a light to decent 6 on this thing, but thats just my opinion. You guys, you were asking for this.. lemme know what you think of this album."]
 
+var commentlist = { comments: []};
+
 export default function commentsection() {
-  var commentlist = []
-  for(let i = 0; i < comments.length; i++){
-    var object = <div>
-                    <h2>
-                      Username
-                    </h2>
-                    <p>
-                    {comments[i]} 
-                    </p>
-                  </div>
-    commentlist.push(object)
+  createComments(commentlist)
+
+  const [inputval, setinputval] = useState('')
+  function handleInput(e) {
+    comments.push(inputval)
+    createComments(commentlist)
+    document.getElementById('submitcomment').value= ''
+    setinputval('')
   }
+
   return (
     <>
     <ul className={styles.container}>
@@ -27,13 +29,33 @@ export default function commentsection() {
         </h1>
       </l>
       <l>
-        <textarea placeholder='Leave a review...' type="text"/>
+        <textarea placeholder='Leave a review...' type="text" id="submitcomment"
+         value={inputval} onChange={(e)=>setinputval(e.target.value)}/>
+        <button onClick={handleInput}> Submit </button>
       </l>
-      <l>
-        {commentlist}
+      <l id="up">
+        {commentlist.comments}
       </l>
     </ul>
-    
     </>
   )
 }
+
+function createComments(commentlist){
+  commentlist.comments = []
+  for(let i = 0; i < comments.length; i++){
+    var object = <div>
+                    <h2>
+                      Username
+                    </h2>
+                    <p>
+                    {comments[i]} 
+                    </p>
+                  </div>
+    commentlist.comments.push(object)
+  }
+  return; 
+}
+
+
+
