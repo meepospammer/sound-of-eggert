a9358a5b2536ab68339847d681a139ec15f8a15c
Elias Berner <theepicelias@hotmail.com>
2022-03-06 23:30:18 -0800

    Changed clickable album component so that the entire component acts as a link
    This makes more sense from a user friendliness aspect

--- pages/components/clickablealbum/clickablealbum.js
+++ pages/components/clickablealbum/clickablealbum.js
@@ -5,31 +5,29 @@ import Link from 'next/link'
 function ClickableAlbum(props){
     return(
         //This is just a general component, we'll have to pass it the album values when we have a working backend
-        <p className={styles.p}>
-            <li className={styles.li}>
-                <img src ={props.src}
-                 width='100px' height='100px'
-                 />
-            </li>
-            <li className={styles.li}>
-                <artist>
-                    {props.artist}
-                    :
-                </artist>
-            </li>
-            <li className={styles.li}>
-                <name>
-                    <Link href='tracklists/gluee'>
-                       {props.name}
-                    </Link>
-                </name>
-            </li>
-            <li className={styles.li}>
-                <rating>
-                    {props.rating}
-                </rating>
-            </li>
-        </p>
+        <Link href={props.link}>
+            <p className={styles.p}>
+                <li className={styles.li}>
+                    <img src ={props.src} href={props.link} width='100px' height='100px' />
+                </li>
+                <li className={styles.li}>
+                    <artist>
+                        {props.artist}
+                        :
+                    </artist>
+                </li>
+                <li className={styles.li}>
+                    <name>
+                        {props.name}
+                    </name>
+                </li>
+                <li className={styles.li}>
+                    <rating>
+                        {props.rating}
+                    </rating>
+                </li>
+            </p>
+        </Link>
     )
 }
 
