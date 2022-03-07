f3449c0510bdd77ae92f7494792e1c5426d975b3
Elias Berner <theepicelias@hotmail.com>
2022-03-06 23:20:10 -0800

    Added pagelink to prop info passed to clickablealbum

--- pages/components/rankinglist/rankinglist.js
+++ pages/components/rankinglist/rankinglist.js
@@ -8,22 +8,22 @@ function rankinglist(){
         <ul className={styles.ul}>
             <ClickableAlbum name="Gluee" artist="Bladee"
              src="https://i1.sndcdn.com/artworks-000068528208-1w4h5h-t500x500.jpg"
-             rating={10.0}/>
+             rating={10.0} link="tracklists/gluee"/>
 
              <ClickableAlbum name="Graduation" artist="Kanye West"
-             src="https://upload.wikimedia.org/wikipedia/en/7/70/Graduation_%28album%29.jpg" rating={8.4}/>
+             src="https://upload.wikimedia.org/wikipedia/en/7/70/Graduation_%28album%29.jpg" rating={8.4} link="tracklists/gluee"/>
 
              <ClickableAlbum name="Red Light" artist="Bladee"
              src="https://upload.wikimedia.org/wikipedia/en/3/3a/Red_Light_Bladee.jpg"
-             rating={7.9}/>
+             rating={7.9} link="tracklists/gluee"/>
 
              <ClickableAlbum name="Donda" artist="Kanye West"
              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Solid_black.svg/1920px-Solid_black.svg.png"
-             rating={7.9}/>
+             rating={7.9} link="tracklists/gluee"/>
 
             <ClickableAlbum name="MBDTF" artist="Kanye West"
              src="https://m.media-amazon.com/images/I/91dHRGIV65L._SL1500_.jpg"
-             rating={6.0}/>
+             rating={6.0} link="tracklists/gluee"/>
         </ul>
     )
 }
