d6c50a4036a10b482a663084f93557ffba544402
Elias Berner <theepicelias@hotmail.com>
2022-03-10 14:38:20 -0800

    Added search bar state update so innput variable can be stored

--- pages/components/searchbar/searchbar.js
+++ pages/components/searchbar/searchbar.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import styles from './searchbar.module.css'
+import { useState } from 'react';
+
 
 function Searchbar(){
+
+    const [inputval, setinputval] = useState('')
+
+     //When search button is clicked the searchbar input is stored in inputval
+     //TODO: link to searchpage and transfer inputval variable to searchpage so we can parse db for matches
+    function handleClick(e){                               
+        console.log(inputval)
+        document.getElementById('search').value= ''
+    }
+
+
     return(
         <>
             <input
                 type='text'
                 placeholder='Search for your favorite music...'
                 className={styles.input}
-                
+                id="search"
+                value={inputval} onChange={(e)=>setinputval(e.target.value)}
             />
-            <button className={styles.btn}>
+            <button className={styles.btn} onClick={handleClick}>
                 Search
             </button>
             
@@ -18,3 +32,4 @@ function Searchbar(){
     )
 }
 export default Searchbar;
+
