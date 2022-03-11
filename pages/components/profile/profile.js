158f43ecc4e16b203c3ede04342018910a4cb638
Elias Berner <theepicelias@hotmail.com>
2022-03-10 17:13:55 -0800

    Added user rated albums to MyProfile

--- pages/components/profile/profile.js
+++ pages/components/profile/profile.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import styles from './profile.module.css'
 import { useUser } from '@auth0/nextjs-auth0';
+import ClickableAlbum from '../clickablealbum/clickablealbum';
+
+var ratedalbums= [{name: "1000 Gecs", artist: "100 Gecs", src: "https://f4.bcbits.com/img/a2604863078_10.jpg", rating: 9.3, link: "tracklists/gluee"},
+                  {name: "Die Lit", artist: "Playboi Carti", src: "https://images.complex.com/complex/image/upload/c_fill,dpr_auto,f_auto,fl_lossy,g_face,q_auto,w_1280/cjswt6lszt0lnyfyz3bd.jpg", rating: 8.4, link:"tracklists/gluee"}]
 
 
 function Profile(){
+
+    var objlist = [];
+    
+    for(let i = 0; i < ratedalbums.length; i++){
+        var obj = <ClickableAlbum name={ratedalbums[i].name} artist={ratedalbums[i].artist}
+               src={ratedalbums[i].src} rating={ratedalbums[i].rating} link={ratedalbums[i].link} />
+        objlist.push(obj)
+    }
+
+
     const { user, error, isLoading } = useUser();
 
     if (isLoading) return <div>Loading...</div>;
@@ -13,7 +27,7 @@ function Profile(){
         // might be better to just scrap profile pictures
     //let users write a small description
     //have users posts at the bottom?
-    return(
+    return(<>
         <p className={styles.profilebg}>
             <p className = {styles.gridcontainer}>
             <div className={styles.profilepic}>
@@ -35,7 +49,13 @@ function Profile(){
                 </li>
             </ ul>
         </p>
+        <h1 className={styles.h1}>
+            Your Album Ratings:
+        </h1>
+        <ul className={styles.ratingsection}>
+            {objlist}
+        </ul>
 
-    )
+    </>)
 }
 export default Profile;
